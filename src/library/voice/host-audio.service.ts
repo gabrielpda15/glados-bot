dd26066ac9b1b5f5978b1b7d3ac3a0a1e0e141fe
Gabriel Pupim <gabriel.pda15@gmail.com>
2023-02-02 15:12:24 -0300

    Removed naudiodon from project

--- src/library/voice/host-audio.service.ts
+++ src/library/voice/host-audio.service.ts
@@ -1,31 +1,22 @@
 import { args } from '@app/main';
 import { createAudioResource, StreamType } from '@discordjs/voice';
-import { getDevices, DeviceInfo } from 'naudiodon';
 import { Microphone } from './microphone.service';
 
 export class HostAudioService {
 	private mic: Microphone;
-	private selectedDevice: DeviceInfo = null;
 
 	private constructor() {}
 
 	public static create() {
 		const service = new HostAudioService();
 
-		let device = args.get('device');
-		if (typeof device === 'string' && device.length > 0) {
-			device = device.toLowerCase().trim();
-			service.selectedDevice = getDevices().find((value) => {
-				const deviceName = value.name.toLowerCase().trim();
-				return deviceName.includes(device as string);
-			});
-		}
+		const device = args?.get('device')?.toString?.()?.toLowerCase().trim();
 
 		service.mic = new Microphone({
 			bitwidth: 16,
 			channels: 2,
 			rate: 44100,
-			device: <any>service.selectedDevice?.name,
+			device: device as any,
 		});
 
 		return service;
